c336232ab959b01715320a2258e7524bd5b01c6c
Strontium233 <metako213@gmail.com>
2019-11-21 15:45:12 -0500

    move files

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import HomePage from './Home'
+import InfoPage from './Info'
 
 function App() {
   return (
     <div>
-        <BrowserRouter>
+        <BrowserRouter basename={window.location.pathname || ''}>
           <Switch> { /* Similar to a switch statement - shows the component depending on the URL path */ }
             { /* Each Route below shows a different component depending on the exact path in the URL  */ }
             <Route exact path='/' component={HomePage}/>
-            {/* <Route exact path='/queue' component={Queue}/> */}
+            <Route exact path='/info' component={InfoPage}/>
           </Switch>
         </BrowserRouter>
+        <div>
+          <p className="HomeDisclaimer" >DISCLAIMER: The content of this web site is entirely the responsibility of a 
+          campus organization which is independent from the University of Toronto. The opinions
+           expressed do not necessarily reflect those of the University. The University of Toronto 
+           shall not be liable for any damage resulting from the use or misuse of the contents of this
+            web site.</p> </div>
       </div>
   );
 }
