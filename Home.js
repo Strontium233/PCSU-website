cadad767db210eada8a11a3acc0c13984606de4f
Jessica He <52612256+Strontium233@users.noreply.github.com>
2019-11-21 15:43:04 -0500

    feat(add): add disclaimer

---
+++ Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Home.css';
+
+class HomePage extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1 className = 'Header'>Hello! Bonjour!</h1>
+                <h2> Who are we? </h2>
+                <p> The Pharmaceutical Chemistry Student Union (PCSU) is a 
+                    student-run organization dedicated to supporting undergraduate students 
+                    registered in the specialist program. First established by students in 2008, 
+                    PCSU is continuously evolving to provide resources and enhance students’ 
+                    university experience, in the forms of class help sessions, academic and career-path 
+                    seminars, and athletic and social events. Additionally, the PCSU acts as a liaison 
+                    for open communication between students and faculty. Any student registered in the 
+                    Pharmaceutical Chemistry program is automatically a member of PCSU, while the team 
+                    of executive members who lead PCSU are elected annually by the student body.</p>
+                <h2> What is pharmaceutical chemistry? </h2>
+                
+            </div>
+        )
+    }
+}
+export default HomePage;
